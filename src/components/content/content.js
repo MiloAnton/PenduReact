c488710e2f8862ddf6c894aa0a768e14acd5f902
MiloAnton <milo.roche@outlook.com>
2021-12-15 19:18:49 +0100

    Compare words + hide letters

--- src/components/content/content.js
+++ src/components/content/content.js
@@ -4,14 +4,46 @@ import { ThemeContext } from "../../context/ThemeContext";
 import Button from "../Button";
 import NavButtons from "../navButtons";
 
-const GOOD_LETTER = "azertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBNéèàâê";
-
 export default function Content() {
+    const [motadev, setMotadev] = useState("");
+    const [guessed, setGuessed] = useState("");
     const {theme} = useContext(ThemeContext);
     const fetchWord = async () => {
         fetch(`https://animalfinderapi.herokuapp.com/word`)
             .then(response => response.json())
-            .then(json => console.log(json.data.word));
+            .then(json => handleData(json.data.word));
+    }
+    function handleData(joliMot) {
+        setMotadev(joliMot);
+        setGuessed(hideWord(joliMot));
+        console.log(joliMot);
+    }
+    function hideWord(str) {
+        let hidden = '';
+        for(let i = 0; i < str.length; i++){
+            hidden += '_';
+        }
+        return hidden;
+    }
+    function prettyGuessed() {
+        let output = '';
+        for(let i = 0; i < guessed.length; i++){
+            output += ` ${guessed[i]} `;
+        }
+        return output;
+    }
+    function compareWords(charToCompare) {
+        let temp = guessed;
+        for(let i = 0; i < motadev.length; i++){
+            if(motadev[i].toUpperCase() === charToCompare.toUpperCase()){
+               temp = setCharAt(temp, i, charToCompare);
+            }
+        }
+        setGuessed(temp);
+    }
+    function setCharAt(str,index,chr) {
+        if(index > str.length-1) return str;
+        return str.substring(0,index) + chr + str.substring(index+1);
     }
     return (
         <div className={theme ? 'contenu light ' : 'contenu dark'}>
@@ -19,19 +51,16 @@ export default function Content() {
             <p>Trouve dont l'animal qu'est caché et engrange des points</p>
             <NavButtons /><Button value={'🔁'} onClick={() => fetchWord()}/>
             {theme}
+            <p>{prettyGuessed(guessed)}</p>
             <input 
                 value="" 
                 type="text" 
                 placeholder="a"
-                onChange={(event) => console.log(event.target.value)}
+                onChange={(event) => compareWords(event.target.value)}
                 style={{
                     width:"15px"
                 }}
             ></input>
         </div>
     )
-}
-
-function countLetters() {
-    
 }
